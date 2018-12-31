b4bb542c21ff4ee21d386ecdba13ed0cc646d679
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-12-31 21:39:58 +0100

    Removes use of build step (minor)

--- bin/card.js
+++ bin/card.js
@@ -1,7 +1,61 @@
 #!/usr/bin/env node
 // 👆 Used to tell Node.js that this is a CLI tool
 
-const fs = require('fs')
-const path = require('path')
-const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8')
-console.log(output)
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('              Geir Gåsodden'),
+  handle: chalk.white('zrrrzzt'),
+  work: chalk.white('Senior Developer at Telemark fylkeskommune'),
+  opensource: chalk.white('Alheimsins ') + chalk.white('*'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('zrrrzzt'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~zrrrzzt'),
+  github: chalk.gray('https://github.com/') + chalk.green('zrrrzzt'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('geirgasodden'),
+  web: chalk.cyan('https://github.com/zrrrzzt'),
+  npx: chalk.red('npx') + ' ' + chalk.white('zrrrzzt'),
+  labelWork: chalk.white.bold('       Work:'),
+  labelOpenSource: chalk.white.bold('Open Source:'),
+  labelTwitter: chalk.white.bold('    Twitter:'),
+  labelnpm: chalk.white.bold('        npm:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelWeb: chalk.white.bold('        Web:'),
+  labelCard: chalk.white.bold('       Card:')
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} / ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const npming = `${data.labelnpm}  ${data.npm}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + // data.name + data.handle
+               newline + newline + // Add one whole blank line
+               working + newline + // data.labelWork + data.work
+               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
+               twittering + newline + // data.labelTwitter + data.twitter
+               npming + newline + // data.labelnpm + data.npm
+               githubing + newline + // data.labelGitHub + data.github
+               linkedining + newline + // data.labelLinkedIn + data.linkedin
+               webing + newline + newline + // data.labelWeb + data.web
+               carding // data.labelCard + data.npx
+
+console.log(chalk.green(boxen(output, options)))
